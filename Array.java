40cd70d7af0564503359b9d851a3640b834e3710
Aditya Chauhan <officialemail032@gmail.com>
2026-01-02 20:59:00 +0530

    Demo commit

---
+++ Array.java
@@ -0,0 +1,11 @@
+public class Array {
+
+    public static void main(String[] args) {
+
+        int n = 25;
+        for(int i=0; i<n; i++){
+            System.out.println(i);
+        }
+    }
+    
+}
