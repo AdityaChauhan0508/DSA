1eedd44f28dcbc5e1a79f752aed330588eee80e7
Aditya Chauhan <officialemail032@gmail.com>
2026-01-30 22:11:54 +0530

    Add implementation for finding the length of the longest subarray with sum zero using HashMap

---
+++ Hashing/larSubarrSumZero.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class larSubarrSumZero {  // O(n) time complexity, O(n) space complexity
+    
+    public static void main(String[] args) {
+        int arr[] = {15, -2, 2, -8, 1, 7, 10, 23};
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        //(sum, index)
+
+        int sum = 0;
+        int len = 0;
+
+        for (int j=0; j<arr.length; j++) {
+            sum += arr[j];
+
+            if (map.containsKey(sum)) {
+                len = Math.max(len, j - map.get(sum));
+            } else {
+                map.put(sum, j);
+            }
+    }
+
+    System.out.println("Length of the longest subarray with sum zero: " + len);
+    }
+}
