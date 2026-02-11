597e1e27d5f54bc7f114f8992eb285b7addd6c48
Aditya Chauhan <officialemail032@gmail.com>
2026-02-11 20:33:46 +0530

    linkedlist

---
+++ LinkedList/circularLL.java
@@ -0,0 +1,49 @@
+// public class circularLL {
+
+//     static class Node {
+//         int data;
+//         Node next;
+//     }
+
+//     static Node addToEmpty(Node last, int data) {
+//         if (last != null)
+//         return last;
+//         Node newNode = new Node();
+//         newNode.data = data;
+//         last = newNode;
+//         newNode.next = last;
+//         return last;
+//     }
+
+//     static Node addFront(Node last, int data) {
+//         if (last == null)
+//         return addToEmpty(last, data);
+//         Node newNode = new Node();
+//         newNode.data = data;
+//         newNode.next = last.next;
+//         last.next = newNode;
+//         return last;
+//     }
+    
+//     static Node addEnd(Node last, int data) {
+//         if (last == null)
+//         return addToEmpty(last, data);
+//         Node newNode = new Node();
+//         newNode.data = data;
+//         newNode.next = last.next;
+//         last.next = newNode;
+//         last = newNode;
+//         return last;
+//     }
+       
+
+
+    
+//     public static void main(String[] args) {
+//         Node last = null;
+//         last = addToEmpty(last, 6);
+//         last = addEnd(last, 8);
+//         last = addFront(last, 2);
+//         System.out.println(last);
+//     }
+// }
