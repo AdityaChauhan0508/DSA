f767e6ef0dc6e7e308a36c9817b7795f8a756e64
Aditya Chauhan <officialemail032@gmail.com>
2026-02-14 19:39:13 +0530

    Add Zig Zag Linked List implementation with add, print, and zigZag methods

---
+++ LinkedList/zigZagLL.java
@@ -0,0 +1,98 @@
+public class zigZagLL {
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Head and Tail in LL
+    public static Node head;
+    public static Node tail;
+
+    // Method to add a new node at the end
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    // Print the Linked List
+    public void print() { // T.C = O(n)
+        if (head == null) {
+            System.out.println("LL is empty");
+            return;
+        }
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("NULL");
+    }
+
+    //Zig Zag LL 
+    public void zigZag(){
+        //find mid
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+
+        //reverse the 2nd half
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node left = head;
+        Node right = prev;
+        Node nextL , nextR;
+
+        //alternate merge - zig zag merge
+        while (left != null && right != null) {
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+
+            left = nextL;
+            right = nextR;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        zigZagLL ll = new zigZagLL();
+        ll.add(1);
+        ll.add(2);
+        ll.add(3);
+        ll.add(4);
+        ll.add(5);
+        ll.add(6);
+        System.out.println("Original Linked List:");
+        ll.print();
+        ll.zigZag();
+        System.out.println("Zig Zag Linked List");
+        ll.print();
+    }
+    
+}
