6f4cb3ce4b9818436ce2d9e112f205a9770a2156
Aditya Chauhan <officialemail032@gmail.com>
2026-02-18 22:12:13 +0530

    Add conditional statement implementation with age and comparison checks

---
+++ Loop/conditional.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+// if || else conditional statement
+
+public class conditional {
+    public static void main(String args[]) {
+        Scanner sc= new Scanner(System.in);
+        int age = sc.nextInt();
+
+        if(age>18){
+            System.out.println("you are adult");
+        }else{
+            System.out.println("you are not adult");
+        }
+
+
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a==b) {
+            System.out.println("a and b is equal");
+        }else{
+            if(a>b){
+                System.out.println("a is greater");
+            }else{
+                System.out.println("a is lesser");
+            } 
+        sc.close();       
+    }
+    }            
+              
+}   
+    
+    
