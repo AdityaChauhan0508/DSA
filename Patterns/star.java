077ecf0b5ef1c825d55a88c02aa7d690cf6ffd4c
Aditya Chauhan <officialemail032@gmail.com>
2026-03-02 22:36:25 +0530

    Add implementation of star pattern printing

---
+++ Patterns/star.java
@@ -0,0 +1,12 @@
+ class star {
+    //printing of stars using output function
+    public static void main(String[] args) {
+        System.out.println("printing of stars");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        
+    }
+}
